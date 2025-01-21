35935599e1ef955ebe390f5491b6312822545235
muhammad <sakhawat.ali.development@gmail.com>
2025-01-21 10:50:42 +0500

    remove the old api

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
-@app.get("/deploy")
-def read_root():
-    return {"message": "deployed yahoo"}
